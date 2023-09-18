fd7476bffc34f08e96d03533e77aed3162a001be
MickaelNeves <nevesmickael8@gmail.com>
2023-09-18 19:41:00 +0100

    Github project link

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -1,6 +1,8 @@
+import CustomLink from "./CustomLink";
+
 const Footer = () => {
   return (
-    <footer className="text-center md:justify-start pt-40 pb-20 text-blue-500 text-sm">
+    <footer className="text-center md:justify-start pt-40 pb-20 text-blue-500 text-sm flex items-center">
       Built with
       <span className="text-white hover:text-blue-800 transition-colors duration-200 ease-in ml-1 font-semibold">
         Next.js
@@ -13,7 +15,14 @@ const Footer = () => {
       <span className="text-white hover:text-blue-800 transition-colors duration-200 ease-in ml-1 font-semibold">
         TypeScript
       </span>
-      . Check the source code here.
+      <span className="mr-2">. Check the source code</span>
+      <CustomLink
+        link="https://github.com/MickaelNeves/v2"
+        controlled={false}
+        newTab
+      >
+        here.
+      </CustomLink>
     </footer>
   );
 };
