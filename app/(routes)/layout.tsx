146dfbbc17d300a474c41291573893ba9d66d719
MickaelNeves <nevesmickael8@gmail.com>
2023-09-18 09:33:33 +0100

    Add GA4

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -1,9 +1,11 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 
 import "@/app/_styles/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+const GTM_ID = "GTM-NFNL9S33";
 
 export const metadata: Metadata = {
   title: "Mickael Neves",
@@ -18,6 +20,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM_ID}');
+        `}
+      </Script>
+      <noscript
+        dangerouslySetInnerHTML={{
+          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
+        }}
+      />
       <body className={inter.className}>
         <main className="mx-auto max-w-screen-lg px-4 min-h-screen">
           {children}
